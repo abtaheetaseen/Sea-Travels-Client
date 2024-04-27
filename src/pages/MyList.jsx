2459a2de0b31920952dd81a33f18c9f8e9aca276
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-04-27 11:16:19 +0600

    fourteenth commit

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../context/AuthProvider'
-import MyListSpot from './MyListSpot';
+import { Link } from 'react-router-dom';
+import { IoEyeSharp } from "react-icons/io5";
+import { FaPen } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 
 const MyList = () => {
 
@@ -18,11 +21,66 @@ const MyList = () => {
     }, [user])
  
   return (
-    <div className='w-10/12 mx-auto my-[50px] grid grid-cols-1 lg:grid-cols-3 gap-10'>
-      {
-        myListSpots.map(spot => <MyListSpot spot={spot} key={spot._id}/>)
-      }
+    
+    <>
+    <div className='min-h-screen flex items-center justify-center'>
+        <div>
+            <h1 className='text-center mb-10 text-5xl font-normal'>My List Length: {myListSpots.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Spot Name</th>
+                            <th>Country</th>
+                            <th>Cost</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            myListSpots.map(spot => <tr key={spot._id}>
+                                
+                                <td>{spot.touristSpotName}</td>
+                                <td>{spot.countryName}</td>
+                                <td>{spot.averageCost}</td>
+                                <td>
+                                <div className='flex items-center justify-start gap-5'>
+    <Link to={`/touristSpots/${spot._id}`}>
+        <button className='mt-5 btn btn-sm btn-primary text-white'>
+        <IoEyeSharp />
+        </button>
+    </Link>
+
+    <Link>
+        <button className='mt-5 btn btn-sm btn-success text-white'>
+        <FaPen />
+        </button>
+    </Link>
+
+    <Link>
+        <button className='mt-5 btn btn-sm btn-error text-white'>
+        <MdDeleteForever />
+        </button>
+    </Link>
     </div>
+                                    </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div className='flex items-center justify-center mt-10'>
+                <Link to="/addTouristSpot">
+                <button className='btn btn-sm btn-neutral'>Add More Spot</button>
+                </Link>
+            
+            </div>
+            </div>
+            </div>
+        </>
   )
 }
 
